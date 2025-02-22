958953a09e712c558e95a9ed92a94bd6790bb887
Lloyd0222 <lowjackcross3157@gmail.com>
2025-02-23 01:25:06 +0900

    Update script.js

--- script.js
+++ script.js
@@ -9,4 +9,16 @@ document.addEventListener("DOMContentLoaded", function() {
             alert("言語切り替えは現在準備中です。");
         });
     });
+
+  // メニューアイコン動作
+  const menuIcon = document.querySelector(".menu-icon");
+  const menuBar = document.querySelector(".menu-bar");
+
+  menuIcon.addEventListener("mouseenter", function () {
+    menuBar.style.display = "flex";
+  });
+
+  menuContainer.addEventListener("mouseleave", function () {
+    menuBar.style.display = "none";
+  });
 });
